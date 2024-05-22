a8a208baf7bf0d2b910205fa1f61030bec718d86
Nico Baier <nico.baier@gmail.com>
2024-05-21 23:30:26 -0500

    refactor(index.ts): remove unused extractMarkdownLinks function to improve code readability and maintainability feat(index.ts): add logging for the number of valid URLs found and the URL being opened for better debugging feat(index.ts): open URLs using a specific browser identifier to provide a more customized browsing experience

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,26 @@
 import { getSelectedText, open, showHUD } from "@raycast/api";
 import isUrl from "is-url-superb";
 
-const extractMarkdownLinks = (text: string) => {
-  const regex = /\[.*?\]\((.*?)\)/;
-  const linkMatch = text.match(regex);
-  return linkMatch ? linkMatch[1] : text;
-};
-
 function extractValidUrls(text: string) {
   const words = text
     .split(" ")
     .map((word) => word.split("\n"))
     .flat();
 
-  console.log(words);
-
-  return words.map((word) => extractMarkdownLinks(word)).filter((word) => isUrl(word));
+  return words.filter((word) => isUrl(word));
 }
 
 export default async function main() {
   try {
     const selectedText = await getSelectedText();
-    // const lines = selectedText.split("\n");
-
     const urls = extractValidUrls(selectedText);
+    console.log("urls:", urls.length);
     console.log(urls);
     if (urls.length > 0) {
       for (const url of urls) {
-        await open(url.trim());
+        console.log(`opening url: ${url}`);
+
+        await open(url, "company.thebrowser.Browser");
       }
     } else {
       await showHUD("No valid URLs found in the selected text");
