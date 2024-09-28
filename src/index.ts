ed65c4e7e5d0081f9d08ac2cc0c09346be55d04a
Nico Baier <nico.baier@gmail.com>
2024-09-28 13:04:01 -0500

    refactor(index.ts): simplify the extraction of valid URLs by using flatMap method for better readability and maintainability. Also, refactor the main function to improve code structure and readability.

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,27 @@ import { getSelectedText, open, showHUD } from "@raycast/api";
 import isUrl from "is-url-superb";
 
 function extractValidUrls(text: string) {
-  const words = text
-    .split(" ")
-    .map((word) => word.split("\n"))
-    .flat();
+	const words = text.split(" ").flatMap((word) => word.split("\n"));
 
-  return words.filter((word) => isUrl(word));
+	return words.filter((word) => isUrl(word));
 }
 
 export default async function main() {
-  try {
-    const selectedText = await getSelectedText();
-    const urls = extractValidUrls(selectedText);
-    console.log("urls:", urls.length);
-    console.log(urls);
-    if (urls.length > 0) {
-      for (const url of urls) {
-        console.log(`opening url: ${url}`);
+	try {
+		const selectedText = await getSelectedText();
+		const urls = extractValidUrls(selectedText);
+		console.log("urls:", urls.length);
+		console.log(urls);
+		if (urls.length > 0) {
+			for (const url of urls) {
+				console.log(`opening url: ${url}`);
 
-        await open(url, "company.thebrowser.Browser");
-      }
-    } else {
-      await showHUD("No valid URLs found in the selected text");
-    }
-  } catch (error) {
-    await showHUD(String(error));
-  }
+				await open(url, "company.thebrowser.Browser");
+			}
+		} else {
+			await showHUD("No valid URLs found in the selected text");
+		}
+	} catch (error) {
+		await showHUD(String(error));
+	}
 }
